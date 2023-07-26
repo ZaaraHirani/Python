fb3e2d256d61eb40a885019938932ef6ba5f5767
ZaaraHirani <139762327+ZaaraHirani@users.noreply.github.com>
2023-07-27 02:11:41 +0530

    Create ecomweb.py
    Front End of E-Commerce Website

---
+++ ecomweb.py
@@ -0,0 +1,37 @@
+#Sign Up
+<title>Sign Up</title>
+<body>
+   <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand">ECOMMERCE</a>
+      <form class="form-inline">
+         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Log In</button>
+      </form>
+   </nav>
+   <div class="container">
+       <form>
+       <div class="form-group row">
+          <label for="inputEmail13" class="col-sm-2 col-form-label">Email</label>
+          <div class="col-sm-10">
+             <input type="email" class="form-control" id="inputEmail13" placeholder="Email">
+          </div>
+      </div>
+      <div class="form-group row">
+          <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+          <div class="col-sm-10">
+              <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+          </div>
+      </div>
+      <div class="form-group row>
+         <label form="inputUsername" class="col-sm-2 col-form-label">Username</label>
+         <div class="col-sm-10">
+            <input type="text" class="form-control" id="inputUsername" placeholder="Username">
+         </div>
+      </div>
+
+      <div class="form-group row">
+         <div class="col-sm-10">
+            <button type="submit" class="btn btn-primary">Sign Up</button>
+            <a href="">Don't have an account? Sign Up here.</a>
+         </div>
+      </div>
+     </form>
